b361df64bbc3400bb59bbe1509193821613d5b1b
wwbel <ww.bel@ya.ru>
2022-11-19 12:40:31 +0300

    init

---
+++ dags/img_crt_send.py
@@ -0,0 +1,37 @@
+"""
+RND DAG for report system
+
+Reads config to determine wich plots to make
+Creating plot images
+Sends images with an email
+"""
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import timedelta, datetime
+from PIL import Image
+import numpy as np
+
+default_arguments = {
+    'owner': 'wwbel',
+    'start_date': datetime(2022, 11, 19),
+    'retries': 1,
+    'retry_delay': timedelta(seconds=5)
+}
+
+def create_img(dots = 300):
+    a = np.random.rand(dots, dots, dots)
+    img = Image.fromarray(a)
+    return img
+
+
+with DAG(
+        "img_crt_send",
+        default_args=default_arguments,
+        schedule_interval='@daily',
+        catchup=False
+) as dag:
+    task_create_img = PythonOperator(
+        task_id='createImg',
+        python_callable=create_img,
+        do_xcom_push=True
+    )
